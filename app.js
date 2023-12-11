c20d34e6f08c81a30e4975b2d5d1ea1b8a92907b
nickel-dime <goepnik@gmail.com>
2023-12-11 17:51:34 -0500

    Fix import formatting and update session options

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
@@ -9,25 +9,25 @@ import session from "express-session";
 import "dotenv/config";
 
 const app = express();
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-    origin: 'https://dapper-biscuit-01c4f7.netlify.app'
-}));
+    origin: process.env.FRONTEND_URL,
+  })
+);
 const sessionOptions = {
-    secret: "any string",
-    resave: false,
-    saveUninitialized: false,
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
 };
 if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-        sameSite: "none",
-        secure: true,
-    };
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
 }
-app.use(
-    session(sessionOptions)
-);
+app.use(session(sessionOptions));
 
 app.use(express.json());
 UserRoutes(app);
@@ -35,5 +35,7 @@ ReviewRoutes(app);
 SongRoutes(app);
 app.listen(process.env.PORT || 4000);
 
-const CONNECTION_STRING = 'mongodb+srv://giuseppi:supersecretpassword@cluster0.ittazzy.mongodb.net/porkify?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/porkify'
+const CONNECTION_STRING =
+  "mongodb+srv://giuseppi:supersecretpassword@cluster0.ittazzy.mongodb.net/porkify?retryWrites=true&w=majority" ||
+  "mongodb://127.0.0.1:27017/porkify";
 mongoose.connect(CONNECTION_STRING);
