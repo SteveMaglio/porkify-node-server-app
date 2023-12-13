ec7b9e5f4bf8a779a5b7207e1c9d918795b0416f
aarohinadkarni <arunadkarni@gmail.com>
2023-12-13 01:16:52 -0500

    yasssassasasssssss

--- reviews/dao.js
+++ reviews/dao.js
@@ -4,8 +4,7 @@ export const findAllReviews = () => model.find();
 export const updateReview = (reviewId, review) =>
   model.updateOne({ _id: reviewId }, { $set: review });
 
-//TODO MAKE THIS QUERY WORK <3
-export const findReviewsByUserId = (userId) => model.find().where("user_id").equals(userId);
+export const findReviewsByUserId = (userId) => model.find({user_id: userId});
 
 export const findReviewById = (reviewId) => model.findById(reviewId);
 export const deleteReview = (reviewId) =>
