2f0390591e6d25602e2c138fb7ed4b0ec940bdf8
SteveMaglio <magliocchino.s@northeastern.edu>
2023-12-14 11:55:55 -0500

    default review info now works

--- reviews/schema.js
+++ reviews/schema.js
@@ -5,8 +5,8 @@ const reviewSchema = new mongoose.Schema({
     favorited:  { type: Boolean, required: true, default : false},
     rating:  { type: String, required: true, default : 5.0},
     body:  {type: String, required: true},
-    is_taken_down:  { type: String, required: true},
-    reason_for_taken_down:  { type: String, required: true},
+    is_taken_down:  { type: Boolean, default : false},
+    reason_for_taken_down:  { type: String, default : ""},
     __v : Number
   },
   { collection: "reviews" });
